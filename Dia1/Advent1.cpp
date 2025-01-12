436f1ea99ef983af6d884a2bc06a8a614ffcd6e7
Mvaovil <mvaovil@upv.edu.es>
2025-01-12 15:37:35 +0100

    Actualizado comentarios

--- Dia1/Advent1.cpp
+++ Dia1/Advent1.cpp
@@ -5,30 +5,41 @@
 
 using namespace std;
 
+// Función recursiva para calcular la distancia entre las dos listas
 int calcularDistancia(const vector<int>& lista1, const vector<int>& lista2, int inicio, int fin) {
+    // El caso base es que si el rango tiene un solo elemento, se calcula la diferencia entre los elementos correspondientes
     if (inicio == fin) {
         return abs(lista1[inicio] - lista2[inicio]);
     }
     
+    // Caso recursivo, que debemos dividir la lista basicamente
     int mitad = (inicio + fin) / 2;
     int distanciaIzquierda = calcularDistancia(lista1, lista2, inicio, mitad);
     int distanciaDerecha = calcularDistancia(lista1, lista2, mitad + 1, fin);
     
+    // Combinar los resultados de las sublistas
     return distanciaIzquierda + distanciaDerecha;
 }
 
+// Ahora ordenamos las listas y llamamos a la función recursiva para calcular la distancia
 int divideYVenceras(vector<int>& lista1, vector<int>& lista2) {
+    // Ordenar las listas antes de calcular las distancias
     sort(lista1.begin(), lista1.end());
     sort(lista2.begin(), lista2.end());
+
+    // Llamar a la función recursiva para calcular la distancia entre las listas
     return calcularDistancia(lista1, lista2, 0, lista1.size() - 1);
 }
 
 int main() {
+    // Definir las dos listas de enteros para comprobar si funciona
     vector<int> lista1 = {3, 4, 2, 1, 3, 3};
     vector<int> lista2 = {4, 3, 5, 3, 9, 3};
     
+    // Llamar a la función divideYVenceras para obtener el resultado
     int resultado = divideYVenceras(lista1, lista2);
     
+    // Imprimir el resultado (debería ser 11)
     cout << "La distancia total es: " << resultado << endl;
     
     return 0;
